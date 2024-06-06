b0ee2858480385e3c5658110745aa572bbd8022c
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-06 16:37:03 +0530

    import export & other js

---
+++ udemyReact/js refresher/importExport/app.js
@@ -0,0 +1,18 @@
+//import {apikey} from "./script.js"
+//console.log(apikey);
+
+// import apikey from "./script.js";
+// console.log(apikey);
+
+// import { apikey, email } from "./script.js";
+// console.log(apikey, email);
+
+// import * as infos from "./script.js";
+// console.log(infos.apikey);
+
+import { apikey, email as officialEmail } from "./script.js";
+console.log(officialEmail);
+
+const arr = ["hi", "bye", "chal", "fot"];
+const objs = arr.map((item) => ({text: item}));
+console.log(objs);
---
+++ udemyReact/js refresher/importExport/script.js
@@ -0,0 +1,7 @@
+//export let apikey = "Saptarsi Debnath"
+
+// export default "debsapt211@gmail.com"
+
+export let apikey = "SaptarsiDebnath/pk98";
+export let email = "debsapt211@gmail.com";
+
