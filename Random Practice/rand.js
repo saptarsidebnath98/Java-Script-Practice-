edabab3458eb8b539ea024192d31d385aa631cc5
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-16 13:29:27 +0530

    js interview qs

--- Random Practice/rand.js
+++ Random Practice/rand.js
@@ -1,16 +1,39 @@
-const num = [1, 2, 3, 4, 1, 5];
-const obj = {};
+// const num = [1, 2, 3, 4, 1, 5];
+// const obj = {};
 
-let objKeys = Object.keys(obj)
+// let objKeys = Object.keys(obj)
 
-for(let item of num){
-    if(!objKeys.includes(item)){
-        obj[item] = 1;
-    }else{
-        return obj[item] += 1; 
-    }
+// for(let item of num){
+//     if(!objKeys.includes(item)){
+//         obj[item] = 1;
+//     }else{
+//         return obj[item] += 1; 
+//     }
+// }
+
+//console.log(obj);
+
+//16-07-2024
+let obj2 = { id: "1", name: "user22", age: "26", work: "programmer" };
+
+// console.log(Object.keys(obj2));
+// console.log(Object.values(obj2));
+// console.log(Object.entries(obj2));
+
+//anagrams
+function isAnagram(one, two){
+    let oneN = one.toLowerCase();
+    let twoN = two.toLowerCase();
+    return [...oneN].sort().join("") === [...twoN].sort().join("");
 }
 
-console.log(obj);
+function rotation(n, arr){
+    for(let i = 0; i < n ; i++){
+        let last = arr.pop();
+        arr.unshift(last);
+        console.log(`rotation: ${i + 1}, lastElem: ${last}, arr: ${arr}`);
+    }
+    return arr;
+}
         
     
